25580ce49035e7234b6e39b4269db05d1ebd822c
Andrew <oldm8reggie@gmail.com>
2023-05-17 15:03:57 +0200

    Just messing around

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/oldManLemon/germanFlashCards/functions"
-	"github.com/rs/zerolog"
+	"github.com/oldManLemon/germanFlashCards/sqlutils"
+	"github.com/oldManLemon/germanFlashCards/structs"
+	"github.com/oldManLemon/germanFlashCards/zlogs"
 )
 
 func main() {
 	//* Setup Logging
-	logger := functions.SetupLogger()
-	logger.Trace().Msg("trace message")
-	logger.Debug().Msg("debug message")
-	logger.Info().Msg("info message")
-	logger.Warn().Msg("warn message")
-	logger.Error().Msg("error message")
-	logger.WithLevel(zerolog.FatalLevel).Msg("fatal message")
-	logger.WithLevel(zerolog.PanicLevel).Msg("panic message")
+	logger := zlogs.SetupLogger()
+	// 	logger.Trace().Msg("trace message")
+	// 	logger.Debug().Msg("debug message")
+	// 	logger.Info().Msg("info message")
+	// 	logger.Warn().Msg("warn message")
+	// 	logger.Error().Msg("error message")
+	// 	logger.WithLevel(zerolog.FatalLevel).Msg("fatal message")
+	// 	logger.WithLevel(zerolog.PanicLevel).Msg("panic message")
+
+	c := structs.Card{Article: "das", WordGerman: "Tempo", WordEnglish: "time"}
+	d := sqlutils.Delete_data()
 }
