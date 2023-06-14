755d96a28dfd8575eef6b9a8001112215be4e6f0
Andrew <oldm8reggie@gmail.com>
2023-06-14 18:26:10 +0200

    Changes to weighted random. Was messing around to see if I could adjust it more to my liking

--- weightedRandom.py
+++ weightedRandom.py
@@ -1,22 +1,25 @@
 import random
 
 # Define the words and their initial weights
-words = {'tisch': 10, 'bier': 5, 'vogel': 5, 'baum': 2}
+# words = {'tisch': 99, 'bier': 50, 'vogel': 32, 'baum': 7}
+words = {'tisch': 80, 'bier': 36,'tanz': 36, 'vogel': 32, 'baum': 7}
 
 # Function to choose a word with weighted randomness
 def choose_word():
     total_weight = sum(words.values())
-    # print("total weight: "+ str(total_weight))
-    rand_weight = random.uniform(0, total_weight)
-    # print("rand_weight: "+str(rand_weight))
+    print("total weight: " + str(total_weight))
+    rand_weight = random.uniform(0, 100)
+    print("rand_weight: " + str(rand_weight))
     weight_sum = 0
-    for word, weight in words.items():
-        # print(word)
-        # print(weight)
+    normalized_words = {word: (weight / total_weight) * 100 for word, weight in words.items()}
+    print(normalized_words)
+    for word, weight in  normalized_words.items():
+        print("weight: " + str(weight))
         weight_sum += weight
-        # print("weight_sum: "+ str(weight_sum))
+        print("weight_sum: " + str(weight_sum))
+        
 
-        if weight_sum >= rand_weight:
+        if weight_sum > rand_weight:
             return word
 
 
@@ -24,10 +27,10 @@ def choose_word():
 # Choose a word with weighted randomness
 for i in range(1000):
     chosen_word = choose_word()
+    # print(chosen_word)
     # print("chosen Word: "+chosen_word)
     with open('readme.txt', 'a') as f:
         f.write(chosen_word+'\n')
-
 
 for w in words.keys():
     with open('readme.txt', 'r') as f:
